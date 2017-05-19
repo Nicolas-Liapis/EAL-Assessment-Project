6e5aa4f51153d6127bda21ec034140834e828545
Nicolas-Liapis <nicolas.liapis.dev@gmail.com>
2017-05-19 12:51:20 +0100

    v0.3
    navigation and reading working. js password protected.

---
+++ js/password.js
@@ -0,0 +1,10 @@
+// const password = getElementById('input').value;
+//
+// alert('password');
+function validatePass(){
+    if(document.getElementById('input').value == 'listereal'){
+        return true;
+    }else{
+        return false;
+    }
+}
