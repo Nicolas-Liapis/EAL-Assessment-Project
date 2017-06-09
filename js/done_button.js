363490476c7d42a76cc29308e66b6cf965d56fa0
Nicolas <nicolas.liapis.dev@gmail.com>
2017-06-09 15:17:42 +0100

    v1.0.1
    Reading complete. check previous commit.

--- js/done_button.js
+++ js/done_button.js
@@ -2,6 +2,7 @@
 var score = 0;
 
 function score1() {
+  document.getElementById('check1').disabled = true;
    if (
     document.getElementById('b1').disabled === false && document.getElementById('b2').disabled === false && document.getElementById('b3').disabled === false && document.getElementById('b4').disabled === false &&
     document.getElementById('b5').disabled === false && document.getElementById('b6').disabled === false && document.getElementById('b7').disabled === false && document.getElementById('b8').disabled === false &&
@@ -15,29 +16,31 @@ function score1() {
   if (document.getElementById('b10').disabled === false){score++;}else{score = score;}
   if (document.getElementById('b16').disabled === false){score++;}else{score = score;}
 
-  alert(score);
+  if (score < 3) {document.getElementById('done1').disabled = true; alert('Student is Step 1');}
+  if (score > 3) {document.getElementById('check1').disabled = false;}
+
 }
 
 function score2() {
+
   if ( $("#drag4").parents("#div1").length == 1 && $("#drag1").parents("#div2").length == 1 && $("#drag5").parents("#div3").length == 1 && $("#drag3").parents("#div4").length == 1 && $("#drag2").parents("#div5").length == 1 && $("#drag9").parents("#div6").length == 1 && $("#drag10").parents("#div7").length == 1 && $("#drag7").parents("#div9").length == 1 && $("#drag8").parents("#div10").length == 1 && $("#drag1").parents("#div2").length == 1 && $("#drag13").parents("#div11").length == 1 && $("#drag14").parents("#div12").length == 1 && $("#drag15").parents("#div13").length == 1 && $("#drag11").parents("#div14").length == 1 && $("#drag12").parents("#div15").length == 1 && $("#drag18").parents("#div16").length == 1 && $("#drag17").parents("#div17").length == 1 && $("#drag16").parents("#div18").length == 1 && $("#drag20").parents("#div19").length == 1 && $("#drag19").parents("#div20").length == 1) {
      score++;
   } else {
      score = score;
   }
-       alert(score);
-}
 
-function score3() {
   if ( $("#drag22").parents("#div21").length == 1 && $("#drag25").parents("#div22").length == 1 && $("#drag23").parents("#div23").length == 1 && $("#drag24").parents("#div24").length == 1 && $("#drag21").parents("#div25").length == 1 && $("#drag29").parents("#div26").length == 1 && $("#drag30").parents("#div27").length == 1 && $("#drag26").parents("#div28").length == 1 && $("#drag27").parents("#div29").length == 1 && $("#drag28").parents("#div30").length == 1 ) {
     score++;
-    alert(score);
   } else {
      score = score;
-     alert(score);
   }
+
+  if (score < 1) {alert('Student is Step 1');}
+  if (score > 1) {document.getElementById('check2').disabled = false;}
 }
 
 function score4() {
+
   var calc1 = document.getElementById('calc1').value;
   var calc2 = document.getElementById('calc2').value;
   var calc3 = document.getElementById('calc3').value;
@@ -48,7 +51,8 @@ function score4() {
   if (calc3 == 21){score++;}else{score = score;}
   if (calc4 == 5){score++;}else{score = score;}
 
-  alert(score);
+  if (score < 3) {alert('Student is Step 1');}
+  if (score > 3) {document.getElementById('check3').disabled = false;}
 }
 
 function score5() {
@@ -59,7 +63,8 @@ function score5() {
   } else {
     score = score;
   }
-  alert(score);
+  if (score < 1) {alert('Student is Step 2');}
+  if (score = 1) {document.getElementById('check4').disabled = false;}
 }
 
 function score6() {
@@ -85,11 +90,13 @@ function score6() {
   if (word9 == 'pencil') {score++;}else {score = score;}
   if (word10 == 'two') {score++;}else {score = score;}
 
-  alert(score);
+  if (score < 9) {document.getElementById('done5').disabled = true; alert('Student is Step 2');}
+  if (score > 9) {document.getElementById('check5').disabled = false;}
 }
 
 
 function score7() {
+
   if( $('#answer1').is(':checked') ) {score++;}
   if( $('#answer2').is(':checked') ) {score++;}
   if( $('#answer3').is(':checked') ) {score++;}
@@ -100,9 +107,7 @@ function score7() {
   if( $('#answer8').is(':checked') ) {score++;}
   if( $('#answer9').is(':checked') ) {score++;}
   if( $('#answer10').is(':checked') ) {score++;}
-  alert(score);
-}
 
-function findScore() {
-  alert('Your score is: ' + score);
+  if (score < 9) {document.getElementById('done6').disabled = true; alert('Student is Step 2');}
+  if (score > 9) {document.getElementById('check6').disabled = false;}
 }
