7dec187215d2fe34dc6524a5f88324eb4bf5c4de
Nicolas Liapis <nicolas.liapis.dev@gmail.com>
2018-04-10 23:59:45 +0100

    Fixed login button with unsecure password

--- js/password.js
+++ js/password.js
@@ -1,8 +1,6 @@
-// const password = getElementById('input').value;
-//
-// alert('password');
+
 function validatePass(){
-    if(document.getElementById('input').value == 'listereal'){
+    if(document.getElementById('input').value === 'listereal'){
         return true;
     }else{
         return false;
